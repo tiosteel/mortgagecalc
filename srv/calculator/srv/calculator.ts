dacfbfcc8c2a22fc8a7cba4efc9c8c52486ceb56
Mikhail Ponomarev <Mikhail.Ponomarev@externals.adidas.com>
2024-07-09 16:42:59 +0300

    closer to default

--- srv/calculator/srv/calculator.ts
+++ srv/calculator/srv/calculator.ts
@@ -1,6 +1,6 @@
 import { ApplicationService } from "@sap/cds";
-import type { EuriborValue } from '#cds-models/db/tables';
-import type { euriborPeriod } from '#cds-models/db/types';
+import type { EuriborValue } from '@mortgagecalc/database/@cds-models/mortgagecalc/db/tables';
+import type { euriborPeriod } from '@mortgagecalc/database/@cds-models/mortgagecalc/db/types';
 
 export class CalculatorService extends ApplicationService {
 
@@ -9,7 +9,8 @@ export class CalculatorService extends ApplicationService {
     }
     
     async getEuribor(day: Date, period: euriborPeriod): Promise<number|undefined> {
-        const { EuriborValues } = await import('#cds-models/db/tables');
+        const { EuriborValues: ServiceEuriborValues } = await import('../@cds-models/CalculatorService/index.js')
+        const { EuriborValues } = await import('@mortgagecalc/database/@cds-models/mortgagecalc/db/tables/index.js');
         const result: EuriborValue|undefined = await SELECT.one.from(EuriborValues).where({day: {'<=': day}});
         
         return result?.[period];
