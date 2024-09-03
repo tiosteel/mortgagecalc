19a1a22b33947b1ec142b20aab106706f98d90e3
Mikhail Ponomarev <mikhail_ponomarev@epam.com>
2024-09-03 18:26:02 +0300

    working reuse package

--- packages/foundation/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QueryHelper = void 0;
-var QueryHelper_1 = require("./lib/QueryHelper");
-Object.defineProperty(exports, "QueryHelper", { enumerable: true, get: function () { return __importDefault(QueryHelper_1).default; } });
-//# sourceMappingURL=index.js.map
